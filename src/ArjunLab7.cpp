ff30e592990b853cad2f99e8df91e5559c462601
arjunkalbag <arjun.kalbag@writopialab.org>
2023-06-29 20:28:54 -0500

    Lab7

--- src/ArjunLab7.cpp
+++ src/ArjunLab7.cpp
@@ -10,19 +10,24 @@
 void setup();
 void loop();
 #line 4 "/Users/student/Desktop/IoT/ArjunLab7/src/ArjunLab7.ino"
-SYSTEM_THREAD(ENABLED);
-
 #define HBUTTON D4
 #define VBUTTON V5
-#define TEMPSENSOR A4
+#define VLED V0
+#define TEMPSENSOR V6
+#define TEMPSENSOR2 V7
+#define NOTIFY_EVENT_CODE "notify"
+#define HARDWARE_NOTIFY_MSG "Hardware button pressed!"
+#define VIRTUAL_NOTIFY_MSG "Virtual button pressed!"
 
+SYSTEM_THREAD(ENABLED);
 #include "oled-wing-adafruit.h"
 OledWingAdafruit display;
+bool hardware_button_pressed = false;
 
 void setup()
 {
-    pinMode(TEMPSENSOR, INPUT);
-    pinMode(D2, OUTPUT);
+    pinMode(A4, INPUT);
+    Serial.begin(9600);
 
     display.setup();
     display.clearDisplay();
@@ -37,32 +42,56 @@ void setup()
 
 void loop()
 {
-
-    Blynk.run();
     uint16_t reading = analogRead(A4);
     double voltage = (reading * 3.3) / 4095.0;
     double temperature = (voltage - 0.5) * 100;
     double ftemp = (temperature * 1.8) + 32;
+
     display.printf("%f C \n", temperature);
     display.println(" ");
     display.printf("%f F \n", ftemp);
     display.println(" ");
     display.display();
-    int buttonState = digitalRead(HBUTTON);
 
-    Blynk.virtualWrite(VBUTTON, buttonState);
+    Blynk.run();
     Blynk.virtualWrite(TEMPSENSOR, temperature);
+    Blynk.virtualWrite(TEMPSENSOR2, ftemp);
+
+    if (display.pressedA())
+    {
+        display.clearDisplay();
+        display.println("Push Sent!");
+        display.display();
+        delay(5000);
+        display.clearDisplay();
+        Blynk.logEvent("push_notifications", "Push!");
+
+    }
+
+
+    int buttonState = digitalRead(HBUTTON);
+    Blynk.virtualWrite(V0, buttonState);
+
+    if (buttonState && !hardware_button_pressed)
+    {
+        Serial.println(HARDWARE_NOTIFY_MSG);
+
+        Blynk.logEvent(NOTIFY_EVENT_CODE, HARDWARE_NOTIFY_MSG);
+        hardware_button_pressed = true;
+    }
+
+    else if (!buttonState && hardware_button_pressed)
+    {
+        hardware_button_pressed = false;
+    }
 }
 
-BLYNK_WRITE(V6)
+BLYNK_WRITE(VBUTTON)
 {
-    uint16_t reading = analogRead(A4);
-    double voltage = (reading * 3.3) / 4095.0;
-    double temperature = (voltage - 0.5) * 100;
-    double ftemp = (temperature * 1.8) + 32;
 
-    if (param.asInt() == 1)
+    if (param.asInt())
     {
-        Blynk.logEvent("push_notifications", "Hey, Blynk push here!");
+        Serial.println(VIRTUAL_NOTIFY_MSG);
+        Blynk.logEvent(NOTIFY_EVENT_CODE, VIRTUAL_NOTIFY_MSG);
     }
 }
